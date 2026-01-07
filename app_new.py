b2c40b1ba015dc12472192f6a1ca03c5bd7ba13b
shug1062 <shug1062@gmail.com>
2026-01-07 17:47:23 +0000

    Add files via upload

---
+++ app_new.py
@@ -0,0 +1,54 @@
+"""
+Silver Jewellery Studio - Material Tracker
+Main application entry point (refactored version)
+"""
+from nicegui import ui
+
+# Import all page functions
+from pages.dashboard import dashboard_page
+from pages.students import students_page, student_detail_page
+from pages.materials import materials_page
+from pages.projects import projects_page
+from pages.purchases import purchases_page
+from pages.payments import payments_page
+
+
+# Register all pages
+@ui.page('/')
+def index():
+    dashboard_page()
+
+
+@ui.page('/students')
+def students():
+    students_page()
+
+
+@ui.page('/student/{student_id}')
+def student_detail(student_id: int):
+    student_detail_page(student_id)
+
+
+@ui.page('/materials')
+def materials():
+    materials_page()
+
+
+@ui.page('/projects')
+def projects():
+    projects_page()
+
+
+@ui.page('/purchases')
+def purchases():
+    purchases_page()
+
+
+@ui.page('/payments')
+def payments():
+    payments_page()
+
+
+# Run the app
+if __name__ in {"__main__", "__mp_main__"}:
+    ui.run(title='Silver Jewellery Studio Tracker', port=8080, reload=False)
